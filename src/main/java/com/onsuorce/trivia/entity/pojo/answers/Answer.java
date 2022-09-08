a4f41a8e5729a334496b5a0b7b5438578ce629b2
OnSuorce <bianchi.57.matteo@gmail.com>
2022-09-09 00:44:11 +0200

    fix

--- src/main/java/com/onsuorce/trivia/entity/pojo/answers/Answer.java
+++ src/main/java/com/onsuorce/trivia/entity/pojo/answers/Answer.java
@@ -3,11 +3,14 @@ package com.onsuorce.trivia.entity.pojo.answers;
 import com.onsuorce.trivia.enums.AnswerTypes;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public abstract class Answer<T> {
 
     private T answer;
     private AnswerTypes type;
+    private List<String> options;
 
     public boolean validateAnswer(T guess){
         return guess.equals(answer);
