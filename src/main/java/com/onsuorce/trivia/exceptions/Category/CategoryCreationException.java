6fd28fc9c3bb921bd8350d238a5043e72afe55d6
Matteo Bianchi <bianchi.57.matteo@gmail.com>
2023-08-18 12:38:30 +0200

    Changed message reason

--- src/main/java/com/onsuorce/trivia/exceptions/Category/CategoryCreationException.java
+++ src/main/java/com/onsuorce/trivia/exceptions/Category/CategoryCreationException.java
@@ -3,7 +3,7 @@ package com.onsuorce.trivia.exceptions.Category;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Duplicate")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Duplicated category")
 public class CategoryCreationException extends RuntimeException{
     public CategoryCreationException(String msg) {
         super(msg);
